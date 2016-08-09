04e40f02abbda66de6e720b22931f2a06846b827
jeena <jeenajohn@jjmacpro.local>
2016-08-09 15:39:16 -0700

    todo.hbs updated to create and read todos

--- app/models/todo.js
+++ app/models/todo.js
@@ -3,8 +3,27 @@ import DS from 'ember-data';
 var attr = DS.attr;
 
 export default DS.Model.extend({
-  
+  isEditing: false,
   text: attr('string'),
-  complete: attr('boolean')
+  complete: attr('boolean'),
+
+  completedClass: Ember.computed('complete', function() {
+    if (this.get('complete') === true) {
+      return `completed`;
+    } else {
+      return ' ';
+    }
+  }),
+
+  editingClass: Ember.computed('isEditing', function() {
+
+    if (this.get('isEditing') === true) {
+      return `editing`;
+    } else {
+      return ' ';
+    }
+
+  })
+
 
 });
